7e6e263bc3df96467a167385c3169d3fdc2ab3ea
Polycarp Masika <masikapolycarp@gmail.com>
2018-03-20 13:20:30 +0300

    fix indentation issue in crwal_persist file

--- app/brain/crawl_persist.js
+++ app/brain/crawl_persist.js
@@ -6,25 +6,25 @@ const entity_model = require('../models/entity');
 async function crawl_persist () {
     const crawl_data = await crawl();
     const records  = [];
-    crawl_data.map(response => {
+    crawl_data.map(async (response) => {
         const { entity } = response;
         const save_obj = {
             entity,
             name:           escape(response.name)                           || "",
             caption:        escape(response.caption)                        || "",
             type:           escape(response.type)                           || "",
-            symptoms:       (typeof response.symptoms === 'object')         ? JSON.stringify(response.symptoms)         : '{}',
-            causes:         (typeof response.causes === 'object')           ? JSON.stringify(response.causes)           : '{}',
-            parse_data:     (typeof response.parse_data === 'object')       ? JSON.stringify(response.parse_data)       : '[]',
-            prevention:     (typeof response.prevention === 'object')       ? JSON.stringify(response.prevention)       : '{}',
-            diagnosis:      (typeof response.diagnosis === 'object')        ? JSON.stringify(response.diagnosis)        : '{}',
-            medication:     (typeof response.medication === 'object')       ? JSON.stringify(response.medication)       : '{}',
-            complications:  (typeof response.complications === 'object')    ? JSON.stringify(response.complications)    : '{}',
-            images:         (typeof response.images === 'object')           ? JSON.stringify(response.images)           : '[]'
+            symptoms:       (typeof response.symptoms === 'object')         ?  JSON.stringify(response.symptoms)         : '{}',
+            causes:         (typeof response.causes === 'object')           ?  JSON.stringify(response.causes)           : '{}',
+            parse_data:     (typeof response.parse_data === 'object')       ?  JSON.stringify(response.parse_data)       : '[]',
+            prevention:     (typeof response.prevention === 'object')       ?  JSON.stringify(response.prevention)       : '{}',
+            diagnosis:      (typeof response.diagnosis === 'object')        ?  JSON.stringify(response.diagnosis)        : '{}',
+            medication:     (typeof response.medication === 'object')       ?  JSON.stringify(response.medication)       : '{}',
+            complications:  (typeof response.complications === 'object')    ?  JSON.stringify(response.complications)    : '{}',
+            images:         (typeof response.images === 'object')           ?  JSON.stringify(response.images)           : '[]'
         };
 
-        entity_model.update_or_save_entity(entity, save_obj)
-            .then(() => records.push(entity));
+        await entity_model.update_or_save_entity(entity, save_obj)
+        records.push(entity);
     });
 
     return records;
